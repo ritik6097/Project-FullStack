36d8c30a72b2d4a1397cc2fb9b37e81b15dc4b3a
pavanvulloju <pavan.vulloju@gmail.com>
2022-08-18 01:12:12 +0530

    frontend-angular begin

--- authentication-service/src/main/java/com/stackroute/controller/UserLoginController.java
+++ authentication-service/src/main/java/com/stackroute/controller/UserLoginController.java
@@ -5,12 +5,10 @@ import com.stackroute.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class UserLoginController {
 
     @Autowired
--- product-webapp/webapp/src/app/app.module.ts
+++ product-webapp/webapp/src/app/app.module.ts
@@ -1,34 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { RegisterUserComponent } from './register-user/register-user.component';
 import { ProfileUserComponent } from './profile-user/profile-user.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RecommendationComponent } from './recommendation/recommendation.component';
-=======
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
->>>>>>> ed66a76d4579e4074b2245460107282960625c0d
+import { SecurityComponent } from './security/security.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     RegisterUserComponent,
     ProfileUserComponent,
-    RecommendationComponent
-=======
+    RecommendationComponent,
     NavbarComponent,
     FooterComponent,
     HomeComponent,
-    AboutComponent
->>>>>>> ed66a76d4579e4074b2245460107282960625c0d
+    AboutComponent,
+    SecurityComponent
+
   ],
   imports: [
     BrowserModule,
