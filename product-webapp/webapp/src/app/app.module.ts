724b2037cce07fe3ac3cadbe9b082b1b90395c35
rakesh <rakeshcpr011@gmail.com>
2022-08-17 14:19:07 +0530

    integration backend frontend

--- product-webapp/webapp/src/app/app.module.ts
+++ product-webapp/webapp/src/app/app.module.ts
@@ -1,37 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule}from '@angular/common/http';
+
+
+
+
+
 import { RegisterUserComponent } from './register-user/register-user.component';
 import { ProfileUserComponent } from './profile-user/profile-user.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-=======
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
->>>>>>> ed66a76d4579e4074b2245460107282960625c0d
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     RegisterUserComponent,
-    ProfileUserComponent
-=======
+    ProfileUserComponent,
     NavbarComponent,
     FooterComponent,
     HomeComponent,
     AboutComponent
->>>>>>> ed66a76d4579e4074b2245460107282960625c0d
+  
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- product-webapp/webapp/src/app/register-user/register-user.component.ts
+++ product-webapp/webapp/src/app/register-user/register-user.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { UserServiceService } from '../user-service.service';
+import { Register } from './Register';
 
 @Component({
   selector: 'app-register-user',
@@ -6,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-user.component.css']
 })
 export class RegisterUserComponent implements OnInit {
-
-  constructor() { }
+register :Register=new Register();
+  constructor(private userService:UserServiceService) { }
 
   ngOnInit(): void {
   }
-
+  saveUser()
+{
+  this.userService.createUser(this.register).subscribe(data=>{
+console.log(data);
+if(data.message){
+  alert(data.message);
+}
+else{
+  alert("successfully registered");
+}
+// alert("successfully registered");
+  },error=>console.log(error));
+}
+  registeruser(){
+    console.log(this.register);
+    this.saveUser();
+  }
 }
