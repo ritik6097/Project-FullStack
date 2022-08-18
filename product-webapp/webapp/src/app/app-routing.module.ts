4cf77c248ff48cf848f7db28ca7c590cbbad02f6
pavanvulloju <pavan.vulloju@gmail.com>
2022-08-18 23:34:51 +0530

    loginform frontend basic done

--- product-webapp/webapp/src/app/app-routing.module.ts
+++ product-webapp/webapp/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ProfileUserComponent } from './profile-user/profile-user.component';
 import { RegisterUserComponent } from './register-user/register-user.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { SecurityComponent } from './security/security.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'about',component:AboutComponent},
   {path:'profile-user',component:ProfileUserComponent},
-  {path:'register-user',component:RegisterUserComponent}
+  {path:'register-user',component:RegisterUserComponent},
+  {path:'login', component:SecurityComponent}
 ];
 
 
