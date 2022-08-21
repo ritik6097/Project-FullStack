ed45d6db9a9f9ae8e3b783024dcb0ee1400ef82b
ritikyadav542000 <ritikyadav542000@gmail.com>
2022-08-21 14:10:22 +0530

    front-end

--- product-webapp/webapp/src/app/app-routing.module.ts
+++ product-webapp/webapp/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { RegisterUserComponent } from './register-user/register-user.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { SecurityComponent } from './security/security.component';
+import { RecommendationComponent } from './recommendation/recommendation.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'about',component:AboutComponent},
   {path:'profile-user',component:ProfileUserComponent},
   {path:'register-user',component:RegisterUserComponent},
-  {path:'login', component:SecurityComponent}
+  {path:'login', component:SecurityComponent},
+  {path:'recommendation',component:RecommendationComponent}
 ];
 
 
---
+++ product-webapp/webapp/src/app/reco.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+import { Model } from './model';
+@Injectable({
+  providedIn: 'root'
+})
+export class RecoService {
+  url: string =
+  "https://localhost:5500/advices";
+    constructor(private _http: HttpClient) {}
+    getAllAlbums(): Observable<Model []> {
+      return this._http.get<Model []>(this.url)
+        .pipe(
+          tap(data =>
+          console.log('All: ' + JSON.stringify(data)))
+        );
+    }
+
+
+}
