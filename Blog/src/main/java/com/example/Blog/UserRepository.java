c0f888ff40343a85c4da7ed241069614b799a59f
b.harsha1080 <b.harsha1080@gmail.com>
2022-08-30 16:23:28 +0530

    added api gateway and config serve

--- Blog/src/main/java/com/example/Blog/UserRepository.java
+++ Blog/src/main/java/com/example/Blog/UserRepository.java
@@ -2,5 +2,8 @@ package com.example.Blog;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface UserRepository extends MongoRepository<User,String> {
+    User getUserById(String id);
 }
