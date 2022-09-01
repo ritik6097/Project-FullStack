52f85c780ba248c53967b3334e0582501c3b2dd8
rakesh <rakeshcpr011@gmail.com>
2022-09-01 11:05:32 +0530

    ui changes and eureka added

--- user-service/src/main/java/com/stackroute/UserServiceApplication.java
+++ user-service/src/main/java/com/stackroute/UserServiceApplication.java
@@ -3,9 +3,10 @@ package com.stackroute;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-
+@EnableDiscoveryClient
 @EnableRabbit
 @SpringBootApplication
 public class UserServiceApplication {
